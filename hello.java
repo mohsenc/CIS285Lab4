0de9ad2ce037e5e5d5b117463037af438c98d9fe
mohsenc <mohsenc@umich.edu>
2021-10-06 00:44:37 -0400

    fixed

--- hello.java
+++ hello.java
@@ -1,10 +1,10 @@
-public class hello
+public class Hello
 {
-  public static void main(string[] args)
+  public static void main(String[] args)
   {
-    for(int i = 1; i <=12;i++)
+    for(int i=1;i<=12;i++)
     {
-      system.out.println("Hello World..." + i);
+      System.out.println("Hello World..."+i);
     }
   }
 }
